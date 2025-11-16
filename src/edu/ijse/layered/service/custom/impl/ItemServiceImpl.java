d8850010c79828aaf67b842671ed49bb968bdcd3
Anjana Thrishakya <anjanathrishakya95@gmail.com>
2025-11-16 13:39:54 +0530

    ItemServiceImpl

--- src/edu/ijse/layered/service/custom/impl/ItemServiceImpl.java
+++ src/edu/ijse/layered/service/custom/impl/ItemServiceImpl.java
@@ -5,6 +5,7 @@
 package edu.ijse.layered.service.custom.impl;
 
 import edu.ijse.layered.dto.ItemDto;
+import edu.ijse.layered.entity.ItemEntity;
 import edu.ijse.layered.service.custom.ItemService;
 import java.util.ArrayList;
 
@@ -16,11 +17,13 @@ public class ItemServiceImpl implements ItemService{
 
     @Override
     public String saveItem(ItemDto itemDto) throws Exception {
+        
         return null;
     }
 
     @Override
     public String updateItem(ItemDto itemDto) throws Exception {
+        ItemEntity entity = getItemEntity(itemDto);
         return null;
     }
 
@@ -31,12 +34,32 @@ public class ItemServiceImpl implements ItemService{
 
     @Override
     public ItemDto searchItem(String code) throws Exception {
+        ItemEntity itemEntity = null;
+        if(itemEntity != null){
+            return getItemDto(itemEntity);
+        }
         return null;
     }
 
     @Override
     public ArrayList<ItemDto> getAll() throws Exception {
-        return null;
+        ArrayList<ItemDto> itemDtos = new ArrayList<>();
+        ArrayList<ItemEntity> itemEntities = null;
+        for (ItemEntity itemEntity : itemEntities) {
+            itemDtos.add(getItemDto(itemEntity));
+        }
+        return itemDtos;
+    }
+    
+    private ItemDto getItemDto(ItemEntity itemEntity){
+        return new ItemDto(itemEntity.getCode(), itemEntity.getDesc(),
+                    itemEntity.getPack(), itemEntity.getPrice(), itemEntity.getQoh());
+    }
+    
+    private ItemEntity getItemEntity(ItemDto itemDto){
+        return new ItemEntity(itemDto.getCode(), 
+                itemDto.getDesc(), itemDto.getPack(), 
+                itemDto.getPrice(), itemDto.getQoh());
     }
     
 }
