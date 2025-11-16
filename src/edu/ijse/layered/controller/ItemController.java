ff4f34d794411019887e1ff990c8d2c2ccb38ece
Anjana Thrishakya <anjanathrishakya95@gmail.com>
2025-11-16 12:38:23 +0530

    Item View, Controller and DTO with DB Configuration

---
+++ src/edu/ijse/layered/controller/ItemController.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edu.ijse.layered.controller;
+
+import edu.ijse.layered.dto.ItemDto;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Anjana
+ */
+public class ItemController {
+    
+    
+    public String saveItem(ItemDto itemDto) throws Exception{
+        return null;
+    }
+    
+    public String updateItem(ItemDto itemDto) throws Exception{
+        return null;
+    }
+    
+    public String deteleItem(String code) throws Exception{
+        return null;
+    }
+    
+    public ArrayList<ItemDto> getAll() throws Exception{
+        return null;
+    }
+    
+    public ItemDto searchItem(String code) throws Exception{
+        return null;
+    }
+    
+}
---
+++ src/edu/ijse/layered/dto/ItemDto.java
@@ -0,0 +1,103 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edu.ijse.layered.dto;
+
+/**
+ *
+ * @author Anjana
+ */
+public class ItemDto {
+    private String code;
+    private String desc;
+    private String pack;
+    private double price;
+    private int qoh;
+
+    public ItemDto() {
+    }
+
+    public ItemDto(String code, String desc, String pack, double price, int qoh) {
+        this.code = code;
+        this.desc = desc;
+        this.pack = pack;
+        this.price = price;
+        this.qoh = qoh;
+    }
+
+    /**
+     * @return the code
+     */
+    public String getCode() {
+        return code;
+    }
+
+    /**
+     * @param code the code to set
+     */
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    /**
+     * @return the desc
+     */
+    public String getDesc() {
+        return desc;
+    }
+
+    /**
+     * @param desc the desc to set
+     */
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    /**
+     * @return the pack
+     */
+    public String getPack() {
+        return pack;
+    }
+
+    /**
+     * @param pack the pack to set
+     */
+    public void setPack(String pack) {
+        this.pack = pack;
+    }
+
+    /**
+     * @return the price
+     */
+    public double getPrice() {
+        return price;
+    }
+
+    /**
+     * @param price the price to set
+     */
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    /**
+     * @return the qoh
+     */
+    public int getQoh() {
+        return qoh;
+    }
+
+    /**
+     * @param qoh the qoh to set
+     */
+    public void setQoh(int qoh) {
+        this.qoh = qoh;
+    }
+
+    @Override
+    public String toString() {
+        return "ItemDto{" + "code=" + code + ", desc=" + desc + ", pack=" + pack + ", price=" + price + ", qoh=" + qoh + '}';
+    }
+}
