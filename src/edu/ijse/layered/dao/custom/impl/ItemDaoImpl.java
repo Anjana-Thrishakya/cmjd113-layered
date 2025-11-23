c9a8cec7d8e067503c2ccfb5fc6239b37e7100ed
Anjana Thrishakya <anjanathrishakya95@gmail.com>
2025-11-23 10:04:20 +0530

    ItemCrud completed

--- src/edu/ijse/layered/dao/custom/impl/ItemDaoImpl.java
+++ src/edu/ijse/layered/dao/custom/impl/ItemDaoImpl.java
@@ -4,9 +4,11 @@
  */
 package edu.ijse.layered.dao.custom.impl;
 
+import edu.ijse.layered.dao.CrudUtil;
 import edu.ijse.layered.dao.custom.ItemDao;
 import edu.ijse.layered.entity.ItemEntity;
 import java.util.ArrayList;
+import java.sql.ResultSet;
 
 /**
  *
@@ -16,27 +18,45 @@ public class ItemDaoImpl implements ItemDao{
 
     @Override
     public boolean save(ItemEntity t) throws Exception {
-        return false;
+        return CrudUtil.executeUpdate("INSERT INTO item VALUES(?,?,?,?,?)",
+                t.getCode(), t.getDesc(), t.getPack(), t.getPrice(), t.getQoh());
     }
 
     @Override
     public boolean update(ItemEntity t) throws Exception {
-        return false;
+        return CrudUtil.executeUpdate("UPDATE item SET Description = ?, PackSize = ?,"
+                + " UnitPrice = ?, QtyOnHand = ? WHERE ItemCode = ?",
+                t.getDesc(), t.getPack(), t.getPrice(), t.getQoh(), t.getCode());
     }
 
     @Override
     public boolean delete(String id) throws Exception {
-        return false;
+        return CrudUtil.executeUpdate("DELETE FROM item WHERE ItemCode = ?", id);
     }
 
     @Override
     public ItemEntity search(String id) throws Exception {
+        ResultSet rst = CrudUtil.executeQuery("SELECT * FROM item WHERE ItemCode = ?", id);
+        if(rst.next()){
+            return new ItemEntity(rst.getString("ItemCode"),
+                    rst.getString("Description"), 
+                    rst.getString("PackSize"),
+                    rst.getDouble("UnitPrice"), rst.getInt("QtyOnHand"));
+        }
         return null;
     }
 
     @Override
     public ArrayList<ItemEntity> getAll() throws Exception {
-        return null;
+        ResultSet rst = CrudUtil.executeQuery("SELECT * FROM item");
+        ArrayList<ItemEntity> itemEntities = new ArrayList<>();
+        while(rst.next()){
+            itemEntities.add(new ItemEntity(rst.getString("ItemCode"),
+                    rst.getString("Description"), 
+                    rst.getString("PackSize"),
+                    rst.getDouble("UnitPrice"), rst.getInt("QtyOnHand")));
+        }
+        return itemEntities;
     }
     
 }
