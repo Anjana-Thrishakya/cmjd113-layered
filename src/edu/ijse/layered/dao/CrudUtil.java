9f28494d26e30e40c1c84401714e654bd0e552f6
Anjana Thrishakya <anjanathrishakya95@gmail.com>
2025-11-23 09:39:45 +0530

    Move CrudUTIL and CrudDao from Service to Dao, Mistakenly created in service

--- src/edu/ijse/layered/dao/CrudUtil.java
+++ src/edu/ijse/layered/dao/CrudUtil.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package edu.ijse.layered.service;
+package edu.ijse.layered.dao;
 
 import edu.ijse.layered.db.DBConnection;
 import java.sql.Connection;
--- src/edu/ijse/layered/dao/CurdDao.java
+++ src/edu/ijse/layered/dao/CurdDao.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
  */
-package edu.ijse.layered.service;
+package edu.ijse.layered.dao;
 
 import edu.ijse.layered.dao.SuperDao;
 import java.util.ArrayList;
