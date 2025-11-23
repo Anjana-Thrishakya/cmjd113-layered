70c0ea4976a5ca873e7b7fc7cbd42ed03bbb597b
Anjana Thrishakya <anjanathrishakya95@gmail.com>
2025-11-23 12:54:01 +0530

    Order Transaction

--- src/edu/ijse/layered/controller/OrderController.java
+++ src/edu/ijse/layered/controller/OrderController.java
@@ -5,13 +5,17 @@
 package edu.ijse.layered.controller;
 
 import edu.ijse.layered.dto.OrderDto;
+import edu.ijse.layered.service.ServiceFactory;
+import edu.ijse.layered.service.custom.OrderService;
 
 /**
  *
  * @author Anjana
  */
 public class OrderController {
+    private OrderService orderService = (OrderService) ServiceFactory.getInstance().getService(ServiceFactory.ServiceType.ORDER);
+    
     public String placeOrder(OrderDto orderDto) throws Exception{
-        return null;
+        return orderService.placeOrder(orderDto);
     }
 }
--- src/edu/ijse/layered/service/custom/impl/OrderServiceImpl.java
+++ src/edu/ijse/layered/service/custom/impl/OrderServiceImpl.java
@@ -4,18 +4,91 @@
  */
 package edu.ijse.layered.service.custom.impl;
 
+import edu.ijse.layered.dao.DaoFactory;
+import edu.ijse.layered.dao.custom.ItemDao;
+import edu.ijse.layered.dao.custom.OrderDao;
+import edu.ijse.layered.dao.custom.OrderDetailDao;
+import edu.ijse.layered.db.DBConnection;
+import edu.ijse.layered.dto.OrderDetailDto;
 import edu.ijse.layered.dto.OrderDto;
+import edu.ijse.layered.entity.ItemEntity;
+import edu.ijse.layered.entity.OrderDetailEntity;
+import edu.ijse.layered.entity.OrderEntity;
 import edu.ijse.layered.service.custom.OrderService;
+import java.sql.Connection;
 
 /**
  *
  * @author Anjana
  */
 public class OrderServiceImpl implements OrderService{
-
+    private OrderDao orderDao = (OrderDao) DaoFactory.getInstance().getDao(DaoFactory.DaoTypes.ORDER);
+    private OrderDetailDao orderDetailDao = (OrderDetailDao) DaoFactory.getInstance().getDao(DaoFactory.DaoTypes.ORDER_DETAIL);
+    private ItemDao itemDao = (ItemDao) DaoFactory.getInstance().getDao(DaoFactory.DaoTypes.ITEM);
+    
     @Override
     public String placeOrder(OrderDto orderDto) throws Exception {
-        return null;
+        Connection connection = DBConnection.getInstance().getConnection();
+        
+        try {
+            connection.setAutoCommit(false);
+            
+            OrderEntity orderEntity = new OrderEntity(orderDto.getOrderId(), 
+                    orderDto.getDate(), orderDto.getCustId());
+            if(orderDao.save(orderEntity)){
+                
+                boolean isOrderDetailSaved = true;
+                
+                for (OrderDetailDto orderDetailDto : orderDto.getOrderDetailDtos()) {
+                    OrderDetailEntity orderDetailEntity = new OrderDetailEntity(orderDto.getOrderId(),
+                            orderDetailDto.getItemCode(), orderDetailDto.getQty(), orderDetailDto.getDiscount());
+                    if(!orderDetailDao.save(orderDetailEntity)){
+                        isOrderDetailSaved = false;
+                    }
+                }
+                
+                if(isOrderDetailSaved){
+                    
+                    boolean isItemUpdated = true;
+                    
+                    for (OrderDetailDto orderDetailDto : orderDto.getOrderDetailDtos()) {
+                        ItemEntity entity = itemDao.search(orderDetailDto.getItemCode());
+                        if(entity != null){
+                            entity.setQoh(entity.getQoh() - orderDetailDto.getQty());
+                            if(!itemDao.update(entity)){
+                                isItemUpdated = false;
+                            }
+                        } else{
+                            connection.rollback();
+                            return "Item not found";
+                        }
+                    }
+                    
+                    if(isItemUpdated){
+                        connection.commit();
+                        return "Success";
+                    } else {
+                        connection.rollback();
+                        return "Item Update Error";
+                    }
+                    
+                } else {
+                    connection.rollback();
+                    return "Order Detail Save Error";
+                }
+                
+            } else {
+               connection.rollback();
+               return "Order Save Error";
+            }
+            
+        } catch (Exception e) {
+            connection.rollback();
+            throw e;
+        } finally {
+            connection.setAutoCommit(true);
+        }
+   
     }
     
 }
